35b960d0ba8d4effdd5223b8d1505c88f44fad3d
randyzhao <zhaohongze@gmail.com>
2013-11-30 23:42:30 -0500

    finish all bugs

--- randy/topology_plotter.py
+++ randy/topology_plotter.py
@@ -4,7 +4,6 @@ Created on Nov 29, 2013
 @author: randyzhao
 '''
 import networkx as nx
-import numpy as np
 import matplotlib.pyplot as plt
 import internetTopology as itopo
 
@@ -12,14 +11,20 @@ def plot_topology(topo):
     G = nx.Graph()
     #add nodes
     for nid, longitude, latitude, node_type in topo.nodes.values():
-        G.add_node(nid, pos=(longitude, latitude), node_type=node_type)
+        ncolor = node_type[0]
+        node_size = 300
+        if len(node_type) != 1:
+            node_size = 1000
+        G.add_node(nid, pos=(longitude, latitude), node_type='', node_size=node_size, node_color=ncolor)
     #add edges
     for source_node_id, dest_node_id, label, edge_type in topo.edges:
         G.add_edge(source_node_id, dest_node_id, label=label, edge_type=edge_type)
     pos = nx.get_node_attributes(G, 'pos')
-    nx.draw(G, pos)
+    node_size = nx.get_node_attributes(G, 'node_size')
+    node_color = nx.get_node_attributes(G, 'node_color')
+    nx.draw(G, pos=pos, node_size=node_size.values(), node_color=node_color.values())
     plt.show()
 
-it = itopo.unmarshell_from_file('topo.txt')
+it = itopo.unmarshell_from_file('graph.txt')
 plot_topology(it)
         
